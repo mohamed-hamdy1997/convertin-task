4aabb1690908305c2b46285c28facf3335c5ee18
mohamedhamdy <hamdy@purple-rain.io>
2024-02-04 04:11:51 +0200

    Command for start application

---
+++ app/Console/Commands/StartApplication.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StartApplication extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'start-application';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'start the application.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Start setup the application.');
+
+        $this->info('Start install composer.');
+        $this->composerInstall();
+
+        $this->info('Start prepare .env file');
+        $this->cpEnvFile();
+
+        $this->info('Start migration.');
+        $this->migrate();
+
+    }
+
+    private function cpEnvFile()
+    {
+        $source = base_path('.env.example');
+        $destination = base_path('.env');
+
+        if (!file_exists($source)) {
+            $this->error('.env.example file does not exist.');
+            return;
+        }
+
+        if (file_exists($destination)) {
+            if (!$this->confirm('Do you want to overwrite the existing .env file?')) {
+                return;
+            }
+        }
+
+        if (copy($source, $destination)) {
+            $this->info('.env file copied successfully.');
+        } else {
+            $this->error('Failed to copy .env file.');
+        }
+    }
+
+    private function composerInstall()
+    {
+        passthru("composer install");
+    }
+
+    private function migrate()
+    {
+        passthru("php artisan migrate");
+    }
+}
